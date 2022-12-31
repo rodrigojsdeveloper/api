dc4d05b8ba2f5532ac9fc1d444149b843a6563fc
Rodrigodeveloperjr <rodrigojsdeveloper@gmail.com>
2022-12-30 21:35:01 -0300

    added user creation service

---
+++ src/services/createUser.service.ts
@@ -0,0 +1,23 @@
+import { userRepository } from "../repositories/user.repository";
+import { IUser } from "../interfaces/user.interface";
+import { User } from "../entities/user.entity";
+import { hash } from "bcrypt";
+
+const createUserService = async (user: IUser): Promise<User> => {
+  const hashedPassword = await hash(user.password, 10);
+
+  const newUser = new User();
+  newUser.name = user.name;
+  newUser.email = user.email;
+  newUser.password = hashedPassword;
+  newUser.is_adm = user.is_adm;
+
+  userRepository.create(newUser);
+  await userRepository.save(newUser);
+
+  Reflect.deleteProperty(newUser, "password");
+
+  return newUser;
+};
+
+export { createUserService };
