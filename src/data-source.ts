c7e34d3b22d868336885219827111e8f54944192
Rodrigodeveloperjr <rodrigojsdeveloper@gmail.com>
2023-01-01 13:59:49 -0300

    jest initial setup

--- src/data-source.ts
+++ src/data-source.ts
@@ -4,16 +4,25 @@ require("dotenv").config();
 
 const port = process.env.POSTGRES_PORT as number | undefined;
 
-const AppDataSource = new DataSource({
-  type: "postgres",
-  host: process.env.POSTGRES_HOST,
-  port: port,
-  username: process.env.POSTGRES_USERNAME,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DATABASE,
-  synchronize: false,
-  entities: [`${__dirname}/**/entities/*.{ts,js}`],
-  migrations: [`${__dirname}/**/migrations/*.{ts,js}`],
-});
+const AppDataSource = new DataSource(
+  process.env.NODE_ENV == "test"
+    ? {
+        type: "sqlite",
+        database: ":memory:",
+        entities: [`${__dirname}/**/entities/*.{ts,js}`],
+        synchronize: true,
+      }
+    : {
+        type: "postgres",
+        host: process.env.POSTGRES_HOST,
+        port: port,
+        username: process.env.POSTGRES_USERNAME,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DATABASE,
+        synchronize: false,
+        entities: [`${__dirname}/**/entities/*.{ts,js}`],
+        migrations: [`${__dirname}/**/migrations/*.{ts,js}`],
+      }
+);
 
 export { AppDataSource };
