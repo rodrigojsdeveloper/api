2299ccacd9b7630f9f5566857c79c2aa2f48954e
Rodrigodeveloperjr <rodrigojsdeveloper@gmail.com>
2023-01-02 12:44:05 -0300

    remove message email

--- src/services/users/createUser.service.ts
+++ src/services/users/createUser.service.ts
@@ -2,7 +2,6 @@ import { userRepository } from "../../repositories/user.repository";
 import { BadRequestError } from "../../errors/badRequest.error";
 import { IUser } from "../../interfaces/user.interface";
 import { User } from "../../entities/user.entity";
-import { createTransport } from "nodemailer";
 import { hash } from "bcrypt";
 
 const createUserService = async (user: IUser): Promise<User> => {
@@ -23,28 +22,6 @@ const createUserService = async (user: IUser): Promise<User> => {
   await userRepository.save(newUser);
 
   Reflect.deleteProperty(newUser, "password");
-
-  const transporter = createTransport({
-    host: "smtp.office365.com",
-    port: 587,
-    secure: false,
-    auth: {
-      user: process.env.SMTP_EMAIL,
-      pass: process.env.SMTP_PASSWORD,
-    },
-  });
-
-  await transporter
-    .sendMail({
-      from: process.env.SMTP_EMAIL,
-      to: user.email,
-      subject: "Created user",
-      html: "Thanks for creating an account on my app",
-    })
-    .catch((err) => {
-      console.error(err);
-      throw new BadRequestError("Error sending email, try again later");
-    });
 
   return newUser;
 };
