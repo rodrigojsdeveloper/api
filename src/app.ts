a0c3729bbd6d77aab6e9cc7f7e936843611c12a9
Rodrigodeveloperjr <rodrigojsdeveloper@gmail.com>
2023-01-02 02:09:52 -0300

    api documentation with swagger added

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,16 @@ import { appRoutes } from "./routes";
 import express from "express";
 import "express-async-errors";
 
+import SwaggerDocs from "../docs/swagger.json";
+import SwaggerUi from "swagger-ui-express";
+
 const app = express();
 app.use(express.json());
 
 appRoutes(app);
 
 app.use(handleErrorMiddleware);
+
+app.use("/docs", SwaggerUi.serve, SwaggerUi.setup(SwaggerDocs));
 
 export { app };
