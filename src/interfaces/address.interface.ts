8466ea2a1c7697f5aaaf2e9732293130e859b245
Rodrigodeveloperjr <rodrigojsdeveloper@gmail.com>
2023-01-03 22:00:11 -0300

    interface of address added

---
+++ src/interfaces/address.interface.ts
@@ -0,0 +1,23 @@
+interface IAddress {
+  zip_code: string;
+  country: string;
+  state: string;
+  city: string;
+  district: string;
+  street: string;
+  number: number;
+  complement: string;
+}
+
+interface IAddressUpdate {
+  zip_code?: string;
+  country?: string;
+  state?: string;
+  city?: string;
+  district?: string;
+  street?: string;
+  number?: number;
+  complement?: string;
+}
+
+export { IAddress, IAddressUpdate };
--- src/interfaces/property.interface.ts
+++ src/interfaces/property.interface.ts
@@ -1,11 +1,15 @@
+import { IAddress, IAddressUpdate } from "./address.interface";
+
 interface IProperty {
   value: number;
   size: number;
+  address: IAddress;
 }
 
 interface IPropertyUpdate {
   value?: number;
   size?: number;
+  address?: IAddressUpdate;
 }
 
 export { IProperty, IPropertyUpdate };
